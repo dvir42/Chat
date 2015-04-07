7ea46c97f3f697d57eb1ca0578c35e74c99227f5
dvir42 <dviros7@gmail.com>
2015-04-07 22:34:54 +0300

    Fixed a stupid mistake

--- src/com/github/dvir42/screenshare/Connect.java
+++ src/com/github/dvir42/screenshare/Connect.java
@@ -7,6 +7,7 @@ import com.github.dvir42.screenshare.graphics.Screen;
 import com.github.dvir42.screenshare.p2p.Message;
 import com.github.dvir42.screenshare.p2p.Peer;
 import com.github.dvir42.screenshare.p2p.exceptions.UnhandledMessageTypeException;
+import com.github.dvir42.screenshare.p2p.handlers.SendString;
 import com.github.dvir42.screenshare.p2p.handlers.ShareScreen;
 import com.github.dvir42.screenshare.p2p.utils.NetworkUtils;
 
@@ -17,6 +18,7 @@ public class Connect {
 	public Connect(String ip, int port) throws UnknownHostException,
 			IOException, UnhandledMessageTypeException {
 		Peer peer = new Peer(NetworkUtils.getAvailablePort());
+		Peer.addHandler(new SendString());
 		screen = new Screen();
 		peer.connectAndSend(Peer.getID(port, ip), new Message(
 				new ShareScreen(), peer.getID()));
