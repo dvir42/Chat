9cfe79bf9641a795288356048a0a6360a04506d3
dvir42 <dviros7@gmail.com>
2015-04-08 18:33:21 +0300

    Started new approach

--- src/com/github/dvir42/screenshare/p2p/handlers/SendString.java
+++ src/com/github/dvir42/screenshare/p2p/handlers/SendString.java
@@ -2,7 +2,6 @@ package com.github.dvir42.screenshare.p2p.handlers;
 
 import java.io.IOException;
 
-import com.github.dvir42.screenshare.Connect;
 import com.github.dvir42.screenshare.p2p.Message;
 import com.github.dvir42.screenshare.p2p.PeerConnection;
 import com.github.dvir42.screenshare.p2p.utils.ArrayUtils;
@@ -13,14 +12,9 @@ public class SendString implements Handler {
 	public void handle(PeerConnection peerConnection, String[] args)
 			throws IOException {
 		PeerConnection newConnection = new PeerConnection(args[args.length - 1]);
-		if (args[0].equalsIgnoreCase("img")) {
-			Connect.screen.setImage(newConnection.receiveImage());
-		} else {
-			System.out.println(ArrayUtils.arrayToString(args, 0,
-					args.length - 1));
-			newConnection.sendMessage(new Message(new Acknowledge(), ""));
-			newConnection.close();
-		}
+		System.out.println(ArrayUtils.arrayToString(args, 0, args.length - 1));
+		newConnection.sendMessage(new Message(new Acknowledge(), ""));
+		newConnection.close();
 	}
 
 	@Override
