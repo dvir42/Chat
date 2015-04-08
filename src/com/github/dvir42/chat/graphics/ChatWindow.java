787c01abb05283d573adb4d5cf65b251133adfa5
dvir42 <dviros7@gmail.com>
2015-04-08 19:44:14 +0300

    Added ChatWindow

---
+++ src/com/github/dvir42/chat/graphics/ChatWindow.java
@@ -0,0 +1,42 @@
+package com.github.dvir42.chat.graphics;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+
+public class ChatWindow extends JPanel {
+
+	private static final long serialVersionUID = 2800344890499501800L;
+
+	private final JTextField input;
+	private final JTextPane chatHistory;
+	private String sent;
+
+	public ChatWindow() {
+		JFrame frame = new JFrame("chat");
+		frame.setSize(600, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.add(this);
+		input = new JTextField();
+		chatHistory = new JTextPane();
+		add(input);
+		add(chatHistory);
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER)
+					sent = input.getText();
+			}
+		});
+	}
+
+	public String getSent() {
+		return sent;
+	}
+
+}
